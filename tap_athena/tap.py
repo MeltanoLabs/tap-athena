b45e978ca35c44a3e41afbe7512ec73bc867536c
Aaron ("AJ") Steers <aj@meltano.com>
2021-09-14 11:40:40 -0700

    use the new default stream class to drive discovery

--- tap_athena/tap.py
+++ tap_athena/tap.py
@@ -12,6 +12,7 @@ class TapAthena(Tap):
     """Athena tap class."""
 
     name = "tap-athena"
+    default_stream_class = AthenaStream
 
     config_jsonschema = th.PropertiesList(
         th.Property("aws_access_key_id", th.StringType, required=True),
@@ -20,18 +21,3 @@ class TapAthena(Tap):
         th.Property("s3_staging_dir", th.StringType, required=True),
         th.Property("schema_name", th.StringType, required=True),
     ).to_dict()
-
-    @property
-    def catalog_dict(self) -> dict:
-        if self.input_catalog:
-            return self.input_catalog
-
-        return AthenaStream.run_discovery(self.config)
-
-    def discover_streams(self) -> List[AthenaStream]:
-        """Return a list of discovered streams."""
-        result: List[AthenaStream] = []
-        for catalog_entry in self.catalog_dict["streams"]:
-            result.append(AthenaStream(self, catalog_entry))
-
-        return result

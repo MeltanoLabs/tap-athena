b8fb66b6d90967d0a6796f279b221d6370769f27
pnadolny13 <patnadolny@gmail.com>
2023-05-09 18:30:02 -0400

    move tests

---
+++ tests/test_core.py
@@ -0,0 +1,11 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+import json
+
+from singer_sdk.testing import get_tap_test_class
+
+from tap_athena.tap import TapAthena
+
+SAMPLE_CONFIG = json.load(open(".secrets/config.json"))
+
+TestTapDynamoDB = get_tap_test_class(tap_class=TapAthena, config=SAMPLE_CONFIG)

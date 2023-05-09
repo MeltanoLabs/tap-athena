d42a6f883cbb3a28cf981710930ed2815a8412dc
pnadolny13 <patnadolny@gmail.com>
2023-05-09 18:21:58 -0400

    update to use base SQL connector and tap

--- tap_athena/client.py
+++ tap_athena/client.py
@@ -1,22 +1,31 @@
 """Custom client handling, including AthenaStream base class."""
 
-import requests
-from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
+import typing as t
 
-from singer_sdk.streams import SQLStream
-import sqlalchemy
+from singer_sdk import SQLConnector, SQLStream
 
+
+class AthenaConnector(SQLConnector):
+    """The connector for SQLite.
+
+    This class handles all DDL and type conversions.
+    """
+
+    allow_temp_tables = False
+    allow_column_alter = False
+    allow_merge_upsert = True
+
+    def get_sqlalchemy_url(self, config: dict[str, t.Any]) -> str:
+        """Generates a SQLAlchemy URL for Athena."""
+        return (
+            f"awsathena+rest://{config['aws_access_key_id']}:"
+            f"{config['aws_secret_access_key']}@athena"
+            f".{config['aws_region']}.amazonaws.com:443/?"
+            f"s3_staging_dir={config['s3_staging_dir']}"
+            f"schema={config['schema_name']}"
+        )
 
 class AthenaStream(SQLStream):
-    """Stream class for Athena streams."""
+    """The Stream class for Athena."""
 
-    @classmethod
-    def get_sqlalchemy_url(cls, tap_config: dict) -> str:
-        return (
-            f"awsathena+rest://{tap_config['aws_access_key_id']}:"
-            f"{tap_config['aws_secret_access_key']}@athena"
-            f".{tap_config['aws_region']}.amazonaws.com:443/"
-            f"{tap_config['schema_name']}?"
-            f"s3_staging_dir={tap_config['s3_staging_dir']}"
-        )
+    connector_class = AthenaConnector

622c5f6303faf60ea1e4cbdb6dd2439ee83e4cc4
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-11-11 14:08:14 -0600

    Pass a catalog dictionary

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,9 +10,10 @@ from singer_sdk.testing import get_tap_test_class
 from tap_athena.tap import TapAthena
 
 SAMPLE_CONFIG = json.loads(Path(".secrets/config.json").read_text())
+CATALOG = json.loads(Path("tests/catalog.json").read_text())
 
 TestTapAthena = get_tap_test_class(
     tap_class=TapAthena,
     config=SAMPLE_CONFIG,
-    catalog="tests/catalog.json",
+    catalog=CATALOG,
 )
